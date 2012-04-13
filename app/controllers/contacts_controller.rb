e732f5d5c57d2c7d11271cf0a128cb9108492c61
Kristian Mandrup <kmandrup@gmail.com>
2012-04-13 13:13:08 +0200

    optimized contacts controller using respond_with

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,57 +1,29 @@
 class ContactsController < ApplicationController
+  respond_to :json, :html
+
   # GET /contacts
   # GET /contacts.json
   def index
-    @contacts = Contact.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @contacts }
-    end
+    respond_with Contact.all
   end
 
   # GET /contacts/1.json
   def show
-    @contact = Contact.find(params[:id])
-
-    respond_to do |format|
-      format.json { render json: @contact }
-    end
+    respond_with Contact.find(params[:id])
   end
 
   # POST /contacts.json
   def create
-    @contact = Contact.new(params[:contact])
-
-    respond_to do |format|
-      if @contact.save
-        format.json { render json: @contact, status: :created, location: @contact }
-      else
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with Contact.create(params[:contact])
   end
 
   # PUT /contacts/1.json
   def update
-    @contact = Contact.find(params[:id])
-
-    respond_to do |format|
-      if @contact.update_attributes(params[:contact])
-        format.json { render json: nil, status: :ok }
-      else
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with Contact.update(params[:id], params[:contact])
   end
 
   # DELETE /contacts/1.json
   def destroy
-    @contact = Contact.find(params[:id])
-    @contact.destroy
-
-    respond_to do |format|
-      format.json { render json: nil, status: :ok }
-    end
+    respond_with Contact.destroy(params[:id])   
   end
 end
